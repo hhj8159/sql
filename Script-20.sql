89e710ca6e7f31f531b412ed821359195eef8619
hhj8159 <hhj8159@gmail.com>
2024-10-21 10:30:33 +0900

    steamsale DB create table 수정

---
+++ Script-20.sql
@@ -0,0 +1,60 @@
+CREATE TABLE tb_user(
+	id NUMBER PRIMARY KEY, 
+	pw varchar2(50), 
+	name varchar2(50), 
+	birth date, 
+	create_date date
+);
+
+CREATE TABLE tb_game(
+	id NUMBER NOT NULL,
+	name varchar2(200),
+	cost NUMBER,
+	create_date date
+);
+
+CREATE TABLE tb_buy(
+	id NUMBER NOT NULL,
+	user_id NUMBER,
+	game_id NUMBER,
+	discount_cost NUMBER,
+	refund_date DATE,
+	create_date date
+);
+
+CREATE TABLE tb_category(
+	id NUMBER NOT NULL,
+	TYPE varchar2(200)
+);
+
+CREATE TABLE tb_gametocategory(
+	game_id NUMBER NOT NULL,
+	category_id number
+);
+
+CREATE TABLE tb_discount(
+	id NUMBER NOT NULL,
+	game_id NUMBER,
+	sale NUMBER,
+	start_date DATE,
+	end_date DATE,
+	create_date date
+);
+
+CREATE TABLE userselectlog(
+	id number,
+	user_id number,
+	game_id number,
+	discount_id number,
+	create_date date
+);
+
+CREATE SEQUENCE SEQ_tb_user;
+CREATE SEQUENCE SEQ_tb_game;
+CREATE SEQUENCE SEQ_tb_buy;
+CREATE SEQUENCE SEQ_tb_category;
+CREATE SEQUENCE SEQ_tb_gametocategory;
+CREATE SEQUENCE SEQ_tb_discount;
+CREATE SEQUENCE SEQ_tb_userselectlog;
+
+
